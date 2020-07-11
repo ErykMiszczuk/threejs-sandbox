4d77e6e5139505ab90ed2f25dfaed0b91ce9443f
Eryk Miszczuk <eryk.miszczuk1996@gmail.com>
2020-07-11 22:47:51 +0200

    You spin me right round boxy right round like record

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const tardisBlue = new THREE.Color("rgb(0, 49, 111)");
-const sunYellow = new THREE.Color("rgb(255, 50, 50)");
+const sunYellow = new THREE.Color("rgb(200, 150, 0)");
 const snowWhite = new THREE.Color("rgb(255, 255, 255)");
 
 const scene = new THREE.Scene();
@@ -11,36 +11,34 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 );
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+    antialias: true
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const cubeGeometry = new THREE.BoxGeometry(2, 3, 4);
 const cubeMaterial = new THREE.MeshPhongMaterial({
-    color: snowWhite,
+    color: sunYellow,
     shininess: 50,
 });
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 scene.add(cube);
 
-const pointLight = new THREE.PointLight(sunYellow, 3);
-pointLight.position.z = 20;
-pointLight.position.y = -10;
-pointLight.position.x = -20;
+const pointLight = new THREE.PointLight(snowWhite, 1.5);
+pointLight.position.set(-20, -10, 20);
 scene.add(pointLight);
 
-const pointLight2 = new THREE.PointLight(sunYellow, 3);
-pointLight2.position.z = 20;
-pointLight2.position.y = 10;
-pointLight2.position.x = 20;
+const pointLight2 = new THREE.PointLight(snowWhite, 1.5); 
+pointLight2.position.set(20, 10, 20);
 scene.add(pointLight2);
 
 camera.position.z = 25;
-cube.rotation.z = 5;
-cube.rotation.y = 2;
 
 function animate() {
     requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.z += 0.05;
     renderer.render(scene, camera);
 }
 
